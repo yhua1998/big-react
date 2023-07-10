2c939de64dd6c90e8e28dced1ccbe5bedc9601ef
yhua1998 <385362330@qq.com>
2023-07-11 05:31:23 +0800

    Add reconciler

---
+++ packages/react-reconciler/src/beginWork.ts
@@ -0,0 +1,6 @@
+import { FiberNode } from "./fiber";
+
+export const beginWork = (fiber: FiberNode): FiberNode | null => {
+  // 递归/递：比较返回子Fiber
+  return null;
+};
--- react-demo/src/main.jsx
+++ react-demo/src/main.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 const jsx = <div>Hello <span>big-react</span></div>
 
 
-// console.log(React)
+console.log(React)
 
-// console.log(jsx)
+console.log(jsx)
